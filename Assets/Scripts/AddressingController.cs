b28fb95fbb1d417036daf9ba8d6641a7a84ad68c
Bo <botbunk@gmail.com>
2018-02-25 18:48:55 -0500

    further cleanup

--- Assets/Scripts/AddressingController.cs
+++ Assets/Scripts/AddressingController.cs
@@ -586,66 +586,6 @@ public class AddressingController : MonoBehaviour {
         }
     }
 
-    /* private IEnumerator FlySuperdogAway(float duration)
-    {
-        CircleCollider2D leftCirc = null, rightCirc = null;
-        #region Get colliders
-        foreach (CircleCollider2D col in Superdog.GetComponentsInChildren<CircleCollider2D>())
-        {
-            if (leftCirc == null)
-            {
-                leftCirc = col;
-            } else
-            {
-                if (col.bounds.center.x < leftCirc.bounds.center.x)
-                {
-                    rightCirc = leftCirc;
-                    leftCirc = col;
-                } else
-                {
-                    rightCirc = col;
-                }
-            }
-        }
-        #endregion
-
-        Vector3 start = Superdog.transform.position;
-        Vector3 left = leftCirc.bounds.center;
-        Vector3 right = rightCirc.bounds.center;
-
-        Vector3 startSize = Superdog.transform.localScale;
-        Vector3 midSize = new Vector3(0f, 1.3f * startSize.y, 1f);
-        Vector3 finalSize = new Vector3(-1.3f * 1.3f * startSize.x, 1.3f * 1.3f * startSize.y, 1f);
-
-        float elapsed = 0f;
-        float p;
-
-        Transform t = Superdog.transform;
-        Vector3 startScale = t.localScale;
-
-        do
-        {
-            p = elapsed / duration;
-            t.position = Vector3.Lerp(start, left, Transition.SmoothLerp(p));
-            t.localScale = Vector3.Lerp(startSize, midSize, p * p);
-            yield return new WaitForEndOfFrame();
-            elapsed += Time.deltaTime;
-        } while (elapsed <= duration);
-
-        elapsed = 0f;
-
-        do
-        {
-            p = elapsed / duration;
-            t.position = Vector3.Lerp(left, right, Transition.SmoothLerp(p));
-            t.localScale = Vector3.Lerp(midSize, finalSize, 1 - (1 - p) * (1 - p));
-            yield return new WaitForEndOfFrame();
-            elapsed += Time.deltaTime;
-        } while (elapsed <= duration);
-
-        Destroy(Superdog);
-    } */
-
     private IEnumerator FadeInLevelComplete(float duration)
     {
         LevelCompleteObject.SetActive(true);
