687914beacf34897e51a8b074e69f9ca34ca3892
raegu14 <raegu14@gmail.com>
2018-10-24 12:59:43 -0400

    build for bit bridge halloween showcase

---
+++ Assets/Cutscenes/Scripts/ResetButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResetButton : MonoBehaviour
+{
+
+    public GameObject ResetCanvas;
+    public Button button;
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(ResetCanvas);
+    }
+
+    public void LoadMainScene()
+    {
+        Object[] allGameObjects = FindObjectsOfType(typeof(GameObject));
+        foreach (Object obj in allGameObjects)
+        {
+            if (obj != ResetCanvas)
+            {
+                Destroy(obj);
+            }
+        }
+        SceneManager.LoadSceneAsync("TitleScene");
+        Destroy(ResetCanvas);
+    }
+}
